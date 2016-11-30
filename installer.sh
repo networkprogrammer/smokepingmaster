968de2e65dcaa349f7de5a085a5b3d149ab0b19b
networkprogrammer <sendtobobinjoseph+github@gmail.com>
2016-11-30 13:57:14 -0800

    Added reset script to reload smokeping

--- installer.sh
+++ installer.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 sudo timedatectl set-timezone America/Los_Angeles
 sudo cp tcpping /usr/local/bin/tcpping
+sudo cp reset /usr/local/bin/reset
 sudo chmod +x /usr/local/bin/tcpping
+sudo chmod +x /usr/local/bin/reset
 sudo apt-get -yqq install smokeping 
 sudo sed -i 's~sendmail =~#sendmail =~g' /etc/smokeping/config.d/pathnames 
 sudo chown smokeping:www-data /etc/smokeping/smokeping_secrets
