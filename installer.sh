77769f764982e51d409cabf9b1a4d397ff93dca2
networkprogrammer <sendtobobinjoseph+github@gmail.com>
2016-11-30 11:39:18 -0800

    Updated installer to set persmissions

--- installer.sh
+++ installer.sh
@@ -3,6 +3,7 @@ sudo cp tcpping /usr/local/bin/tcpping
 sudo chmod +x /usr/local/bin/tcpping
 sudo apt-get -yqq install smokeping 
 sudo sed -i 's~sendmail =~#sendmail =~g' /etc/smokeping/config.d/pathnames 
+sudo chown smokeping:www-data /etc/smokeping/smokeping_secrets
 cd /etc/apache2/conf-available
 sudo a2enconf smokeping
 sudo a2enmod cgid
